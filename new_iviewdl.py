009bb240429d64ef626e393ae358d182eb8f8063
blha303 <stevensmith.ome@gmail.com>
2018-09-21 15:27:45 +0800

    Updated again

--- new_iviewdl.py
+++ new_iviewdl.py
@@ -7,14 +7,23 @@ import json
 import argparse
 from youtube_dl import YoutubeDL
 
+def download_url_list(urllist):
+    with YoutubeDL({"outtmpl": "%(series)s %(season_number)sx%(episode_number)s %(title)s.%(ext)s", "external_downloader": "ffmpeg", "external_downloader_args": ["-v", "quiet", "-stats"]}) as ydl:
+        ydl.download(urllist)
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("seriesurl", help="The url to the series you'd like to download, e.g https://iview.abc.net.au/show/shaun-micallefs-mad-as-hell/series/9")
     args = parser.parse_args()
-    page = urlopen(args.seriesurl).read().split(b"window.__INITIAL_STATE__ = ",1)[1].split(b";")[0]
-    data = json.loads(eval(page))
-    with YoutubeDL({"outtmpl": "%(series)s %(season_number)sx%(episode_number)s %(title)s.%(ext)s", "external_downloader": "ffmpeg", "external_downloader_args": ["-v", "quiet", "-stats"]}) as ydl:
-        ydl.download([ep["shareUrl"] for ep in data["page"]["pageData"]["_embedded"]["selectedSeries"]["_embedded"]["videoEpisodes"]])
+    if "/video/" in args.seriesurl:
+        download_url_list([args.seriesurl])
+    else:
+        page = urlopen(args.seriesurl).read().split(b"window.__INITIAL_STATE__ = ",1)[1].split(b";")[0]
+        data = json.loads(eval(page))
+        download_url_list([ep["shareUrl"] for ep in data["page"]["pageData"]["_embedded"]["selectedSeries"]["_embedded"]["videoEpisodes"]])
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Aborted.")
